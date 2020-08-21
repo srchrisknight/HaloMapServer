310be9bedc6665cf152c27af7b20b313666b3db5
MapServer <srchrisknight@gmail.com>
2020-08-21 23:43:46 +0100

    Chaned Message

--- server/hello.py
+++ server/hello.py
@@ -5,4 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return '''
+<h2>Hey Brad!,</h2>
+<p>Im running on a raspberry pi hanging off of Monas desk!</p> 
+<a href="https://photos.app.goo.gl/1TqwaGiVPQgjFjf4A"'>Click Here</a>
+'''
